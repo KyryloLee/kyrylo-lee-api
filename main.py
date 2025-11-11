1cdf0cb454754c8dd2f4573c6f0d8a6a3b1212d9
Kyrylo Lee <kyrylolee@gmail.com>
2025-11-11 16:27:25 +0200

    [ADD] /about endpoint

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello! My name is Kyrylo Lee."}
+
+@app.get("/about")
+async def get_dev():
+    return {"message": ("The goal is to design and implement a robust, "
+                        "high-performance REST API, strictly adhering "
+                        "to strong backend development practices relevant "
+                        "to a microservices architecture, for the purpose "
+                        "of serving personal and professional data.")}
