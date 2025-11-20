1c869afb91dacebbb7e31ca275738f29daaf72af
Kyrylo Lee <kyrylolee@gmail.com>
2025-11-20 17:23:31 +0200

    [ADD] postgresql support

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-from fastapi import FastAPI
+import os
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from sqlmodel import Field, SQLModel, create_engine
+
+ENGINE = None
+
+class Users(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_name: str
+    nick_name: str
+
+
+def create_db_and_tables() -> None:
+    load_dotenv()
+    db_url = os.getenv('DEV_POSTGRES_URL')
+    db_url = db_url.replace('postgres:', 'postgresql+psycopg2:')
+    engine = create_engine(db_url, echo=True)
+    SQLModel.metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def lifspan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+load_dotenv() # loading the ".env" file
+app = FastAPI(lifespan=lifspan)
 
 
 @app.get("/")
