682dba78d5f0a32771c7fff0f5f1ec217ce75dfa
Kyrylo Lee <kyrylolee@gmail.com>
2025-11-21 20:14:26 +0200

    [UPDATE] gitignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+from typing import Dict, Any
 from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
@@ -28,7 +29,7 @@ async def root():
 
 
 @app.get("/about")
-async def get_dev():
+async def get_dev() -> Dict[str, Any]:
     return {
         "id": get_uuid(),
         "message": ("The goal is to design and implement a robust, "
